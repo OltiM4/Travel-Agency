4b4bb77e184b60696b860a27bc238e859b989f22
OltiM4 <149538501+OltiM4@users.noreply.github.com>
2025-01-14 21:18:39 +0100

    Modifikimi klases UnitOfWork

---
+++ UnitOfWork/UnitOfWork.php
@@ -0,0 +1,43 @@
+<?php
+
+include_once 'DConnection.php';
+
+class UnitOfWork implements IUnitOfWork {
+    private $context;
+    private $repositories = [];
+
+    public function __construct() {
+        $this->context = DConnection::getConnection();
+    }
+
+    public function getContext() {
+        return $this->context;
+    }
+
+    public function getRepository($entity) {
+        if (!isset($this->repositories[$entity])) {
+            $this->repositories[$entity] = new $entity($this->context);
+        }
+        return $this->repositories[$entity];
+    }
+
+    public function saveChanges() {
+        try {
+            $this->context->beginTransaction();
+            $this->context->commit();
+        } catch (Exception $e) {
+            $this->context->rollBack();
+            throw $e;
+        }
+    }
+
+    public function saveChangesAsync() {
+        return $this->saveChanges();
+    }
+
+    public function dispose() {
+        $this->context = null;
+    }
+}
+
+?>
